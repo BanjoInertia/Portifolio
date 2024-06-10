5ab104c6f896688797897df5d2a185e44f679925
BanjoInertia <nozembro@gmail.com>
2024-06-10 13:20:14 -0300

    alterações nas seções 2 e 3

--- src/components/Section2/Section2.jsx
+++ src/components/Section2/Section2.jsx
@@ -5,7 +5,12 @@ export const Section2 = () => {
     return (
         <div className={classes.info_container}>
             <h1 className={classes.info_tittle}>SOBRE</h1>
-            <p className={classes.info_text}></p>
+            <p className={classes.info_text}>Meu nome é Marcelo, sou estudante da área de programação frontend, comecei há pouco mais de um ano, e venho sempre procurando melhorar e aprender coisas novas.<br /><br />
+
+            Busco na criação de meus projetos, sempre tentar compilar a maior quantidade de conhecimentos, da forma mais simples possível, para que eu possa demonstrar meus aprendizados, sem que seja necessário dividi-los em vários diferentes projetos.<br /><br />
+
+            Sobre mim, gosto de jogos, principalmente os que envolvem uma história complexa e com personagens interessantes, alguns deles até me fazem refletir sobre a vida, e como ela deve ser seguida.
+            </p>
             <h1 className={classes.info_tittle}>LINGUAGENS</h1>
             <ul className={classes.jofe_container}>
                 <li>
@@ -18,7 +23,7 @@ export const Section2 = () => {
                 </li>
                 <li>
                     <img className={classes.img} src={img}/>
-                    <p className={classes.text} >abcd 1</p>
+                    <p className={classes.text}>abcd 1</p>
                 </li>
                 <li>
                     <img className={classes.img} src={img}/>
--- src/components/Section3/Section3.jsx
+++ src/components/Section3/Section3.jsx
@@ -1,14 +1,22 @@
 import classes from './Section3.module.css'
-import img from '../../assets/Semtulo.png'
+import img1 from '../../assets/Semtulo.png'
+import img2 from '../../assets/jofe.png'
 
 export const Section3 = () => {
     return (
         <>
-            <h1 className={classes.projects_tittle}>PROJETOS</h1>
+            <h1 className={classes.projects_title}>PROJETOS</h1>
             <div className={classes.projects_container}>
-                <div className={classes.projects}><img src={img} className={classes.projects_img} /></div>
-                <div className={classes.projects}><img src={img} className={classes.projects_img} /></div>
+                <div className={classes.projects}>
+                    <img src={img1} className={classes.projects_img} />
+                    <span className={classes.projects_text}>STORE é um <span className={classes.ecommerce}>e-commerce</span> que vende produtos ficticios.</span>
+
+                </div>
+                <a href='https://infomon-two.vercel.app' target='_blank' className={classes.projects}>
+                    <img src={img2} className={classes.projects_img} />
+                    <span className={classes.projects_text}>Infomon é um site de consulta de pokemons.</span>
+                </a>
             </div>
         </>
-    )
-}
+    );
+};
